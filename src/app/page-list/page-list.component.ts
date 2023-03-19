3f95db26b968e518e141249bbdee1f28a72d2076
Mike Meier <m0.0m58@yahoo.de>
2023-03-19 13:11:50 +0100

    todo app done

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,50 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Todo } from '../_interface/todo';
 import { Eventping } from '../_interface/eventping';
 
 @Component({
   selector: 'bm-page-list',
   templateUrl: './page-list.component.html',
-  styleUrls: ['./page-list.component.sass']
+  styleUrls: ['./page-list.component.sass'],
 })
-export class PageListComponent {
+export class PageListComponent implements OnInit {
   public toDoDoneShow: boolean;
   public toDoShow: boolean;
   public $todos: Todo[];
   public $todosDone: Todo[];
-  constructor()  {
+  public $todosLength: any;
+  ngOnInit(): void {
+    
+  }
+  constructor() {
     this.toDoShow = true;
     this.toDoDoneShow = false;
-    this.$todos = [
-      {
-      id: 1, 
-      label: 'Rechnungen schreiben', 
-      status: false, 
-      position: 1,
-      },
-      {
-        id: 1, 
-        label: 'Kochen', 
-        status: false, 
-        position: 1,
-        }
-    ];
-    this.$todosDone = [
-      {
-      id: 1, 
-      label: 'Einkaufen', 
-      status: false, 
-      position: 1,
-      }
-    ];
+    this.$todos = [];
+    this.$todosDone = [];
+    this.$todosLength = 0; 
+    this.loadTodosFromStorage();
   }
+  public loadTodosFromStorage(): void {
+    const storedTodos = localStorage.getItem('todos');
+    const storedTodosDone = localStorage.getItem('todosDone')
+    if (storedTodos !== null) {
+      const parsedTodos = JSON.parse(storedTodos) as Todo[];
+      this.$todos.push(...parsedTodos);
+    }
+    if (storedTodosDone !== null) {
+      const parsedTodosDone = JSON.parse(storedTodosDone) as Todo[];
+      this.$todosDone.push(...parsedTodosDone);
+    }
+  }
+
+  public pushInStorage(): void {
+    const todosAsString = JSON.stringify(this.$todos);
+    const todosDoneAsString = JSON.stringify(this.$todosDone);
+    localStorage.setItem('todos', todosAsString);
+    localStorage.setItem('todosDone', todosDoneAsString);
+  }
+
   public create(event: Todo): void {
-    event.position = this.$todos.length + 1; 
+    event.position = this.$todosLength;
+    this.$todosLength++;
     this.$todos.push(event);
+    this.pushInStorage();
   }
   public update(event: Eventping) {
-    if('check' === event.label) {
-      if(!event.object.status) {
+    if ('check' === event.label) {
+      if (!event.object.status) {
         this.$todosDone.splice(this.$todosDone.indexOf(event.object), 1);
         this.$todos.push(event.object);
       } else {
@@ -52,27 +60,22 @@ export class PageListComponent {
         this.$todosDone.push(event.object);
       }
     }
-    if('delete' === event.label) {
-      if(event.object.status) {
+    if ('delete' === event.label) {
+      if (event.object.status) {
         this.$todosDone.splice(this.$todosDone.indexOf(event.object), 1);
       } else {
         this.$todos.splice(this.$todos.indexOf(event.object), 1);
       }
     }
-    if('label' === event.label) {
-      if(event.object.status) {
+    if ('label' === event.label) {
+      if (event.object.status) {
         this.$todosDone.forEach((toDo: Todo) => {
-          if(toDo.id === event.object.id) {
+          if (toDo.id === event.object.id) {
             toDo.label = event.object.label;
-          }
-        });
-      } else {
-        this.$todos.forEach((todo: Todo) => {
-          if(todo.id === event.object.id) {
-            todo.label = event.object.label;
           }
         });
       }
     }
+    this.pushInStorage();
   }
 }
